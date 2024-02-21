b4a5254f22c9c076c0b038f899ce1bd7bcf50f93
githukueliud <githukueliud@gmail.com>
2024-02-21 05:28:24 -0600

    Login screen UI done

--- app/src/main/java/com/example/chamayetu/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/chamayetu/presentation/HomeScreen.kt
@@ -1,26 +1,63 @@
 package com.example.chamayetu.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Welcome to our chama")
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "What would you like to do today?")
+        Text(
+            text = "Third Wave Chama",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier =Modifier.height(5.dp))
+        Text(
+            text = "Every coin counts",
+            fontSize = 18.sp,
+            color = Color.Green
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "Hi Eliud!",
+            modifier = Modifier.fillMaxWidth(),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row{
+            Text(
+                text = "Your total savings is: Kes ",
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.width(5.dp))
+            Text(
+                text = "53,500",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        }
     }
 }
 
