e40b132c1a3f4b4f9df3c267a72ad406c2b103a7
githukueliud <githukueliud@gmail.com>
2024-02-24 08:32:43 -0600

    Fines screen UI done

--- app/src/main/java/com/example/chamayetu/presentation/FinesScreen.kt
+++ app/src/main/java/com/example/chamayetu/presentation/FinesScreen.kt
@@ -1,10 +1,149 @@
 package com.example.chamayetu.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FinesScreen(modifier: Modifier = Modifier) {
-    Text(text = "Hello from fines")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Third Wave Chama",
+            fontSize = 32.sp
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(80.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(44, 198, 123)
+            )
+        ) {
+            Text(
+                text = "Pay All Fines",
+                fontSize = 28.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+//        Text(
+//            text = "You have no outstanding fines",
+//            fontSize = 25.sp
+//        )
+        //if there is fines are present the UI should be
+        Card(
+            modifier = Modifier
+                .height(220.dp)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Loan Title",
+                    fontSize = 20.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Ksh. 300 is a fine for failure to pay your monthly contribution for week 10 of 2024. The loan should be paid by week 15 to avoid further penalties.",
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(0.8f),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(44, 198, 123)
+                    )
+                ) {
+                    Text(
+                        text = "Pay This Fine",
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Card(
+            modifier = Modifier
+                .height(220.dp)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Loan Title",
+                    fontSize = 20.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Text(
+                    text = "Ksh. 300 is a fine for failure to pay your monthly contribution for week 10 of 2024. The loan should be paid by week 15 to avoid further penalties.",
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .height(80.dp)
+                        .fillMaxWidth(0.8f),
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(44, 198, 123)
+                    )
+                ) {
+                    Text(
+                        text = "Pay This Fine",
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun FinesScreenPreview() {
+    FinesScreen()
 }
